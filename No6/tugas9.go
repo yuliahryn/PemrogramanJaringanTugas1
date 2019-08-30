cee093cc5c2ebb09e2007d76691baf7365fa93e9
yuliahryn <54700740+yuliahryn@users.noreply.github.com>
2019-08-30 03:03:34 -0400

    Add files via upload

---
+++ No6/tugas9.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+func main() {
+	http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprintf(w, "Hello, youve requested:%s\n", r.URL.Path)
+	})
+	http.ListenAndServe(":8000", nil)
+}
